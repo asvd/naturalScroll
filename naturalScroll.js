a4a0cdc9ad2bca11094ec5dff76ec4f0cbaa70b8
Dmitry Prokashev <heliosframework@gmail.com>
2015-04-07 00:28:59 +0200

    compression

--- naturalScroll.js
+++ naturalScroll.js
@@ -21,6 +21,14 @@ function (exports) {
     var DELAY = 20;
     var TIME = 600;
 
+    // better compression
+    var null_ = null;
+    var addEventListener = 'addEventListener';
+    var Math_ = Math;
+
+    var scrollTop = 'scrollTop';
+    var scrollLeft = 'scrollLeft';
+
 
     /**
      * Calculates the animation and uses the provided setter function
@@ -29,262 +37,211 @@ function (exports) {
      * @param {Function} setter to apply the animation frame
      */
     var Animator = function(setter) {
-        this.__setter = setter;
-        this.__f = [[0,0,0,0,0,0]];  // frames and diffs
-        this.__fIdx = 0;             // current frame number
-        this.__interval = null;
-    }
-    
+        var F = [[0,0,0,0,0,0]];  // frames and diffs
+        var fIdx = 0;             // current frame number
+        var interval = null_;
 
-    /**
-     * Animates to the desired position within the given time
-     * 
-     * @param {Number} target
-     * @param {Number} time (msec);
-     */
-    Animator.prototype.slide = function(target, time) {
-        var fnum = Math.floor(time / DELAY);
-
-        // prevent animation run slover than the current one
-        if (this.__interval !== null) {
-            var start = this.__f[0][0];
-            var current = this.__f[this.__fIdx][0];
-            var dist = current - start;
-            var speed = Math.abs(dist / this.__fIdx);
-
-            if (speed > 0) {
-                var newFnum = Math.abs(
-                    Math.round((target - current) / speed)
-                );
-
-                fnum = Math.max(
-                    5, Math.min( fnum, newFnum )
-                );
-            }
-        }
-
-        var frame = this.__f[this.__fIdx];
-        this.__f = this.__calculateFrames(
-            frame[0], frame[1], frame[2], target, fnum
-        );
-
-        this.__fIdx = 0;
-
-        if (!this.__interval) {
-            this.__start();
-        }
-    }
-
-
-    /**
-     * Updates the value changed from outside
-     * 
-     * @param {Number} value
-     */
-    Animator.prototype.update = function(value) {
-        if (this.__interval === null) {
-            this.__f[this.__fIdx][0] = value;
-        }
-    }
-
-
-    /**
-     * Starts the animation
-     */
-    Animator.prototype.__start = function() {
-        var me = this;
-
-        var tick = function() {
-            var val = me.__f[me.__fIdx++][0];
-            me.__setter(val);
-            
-            if (me.__fIdx == me.__f.length) {
-                clearInterval(me.__interval);
-                me.__interval = null;
-                me.__f = [[val,0,0,0,0,0]];
-                me.__fIdx = 0;
-            }
-        }
-
-        this.__interval = setInterval(tick, DELAY);
-    }
-    
-    
-
-    /**
-     * Integer-based calculation of the animation frames spline
-     * 
-     * @param {Number} f0 current value
-     * @param {Number} f1 current first difference
-     * @param {Number} f2 current second difference
-     * @param {Number} target value at the end
-     * @param {Number} fnum number of frames
-     */
-    Animator.prototype.__calculateFrames = function(
-        f0, f1, f2, target, fnum
-    ) {
-        var n = fnum;
-        var n2 = n * n;
-        var n3 = n2 * n;
-        var n4 = n3 * n;
-        var n5 = n4 * n;
-
-        var f3, f4, f5;
-
-        // these magic formulae came from outer space
-        f3 = -( 9 * f2 * n2 +
-                (36 * f1 -9 * f2) * n -
-                36 * f1 +
-                60 * f0 -
-                60 * target
-              ) / (n3 - n);
         
-        f4 = ( 36 * f2 * n2 +
-               (192 * f1 -36 * f2) * n -
-               192 * f1 +
-               360 * f0 -
-               360 * target
-             ) / (
-                 n4 +
-                 2 * n3 -
-                 n2 -
-                 2 * n
-             );
-        
-        f5 = -( 60 * f2 * n2 +
-                (360 * f1 -60 * f2) * n -
-                360 * f1 +
-                720 * f0 -
-                720 * target
-              ) / (
-                  n5 +
-                  5 * n4 +
-                  5 * n3 -
-                  5 * n2 -
-                  6 * n
-              );
+        /**
+         * Animates to the desired position within the given time
+         * 
+         * @param {Number} target
+         * @param {Number} time (msec);
+         */
+        this.s = function(target, time) {
+            var fnum = Math.floor(time / DELAY);
+            var frame = F[fIdx];
 
-        var result = [[f0, f1, f2, f3, f4, f5]];
+            // prevent animation run slover than the current one
+            if (interval !== null_) {
+                var start = F[0][0];
+                var current = frame[0];
+                var dist = current - start;
+                var speed = Math_.abs(dist / fIdx);
 
-        var lastframe, frame, i, j;
-        for (i = 0; i < fnum; i++) {
-            lastframe = result[result.length - 1];
+                if (speed > 0) {
+                    var newFnum = Math_.abs(
+                        Math_.round((target - current) / speed)
+                    );
 
-            frame = [0, 0, 0, 0, 0, f5];
-
-            for (j = 4; j >= 0; j--) {
-                frame[j] = frame[j+1] + lastframe[j];
+                    fnum = Math_.max(
+                        5, Math_.min( fnum, newFnum )
+                    );
+                }
             }
 
-            result.push(frame);
-        }
-
-        return result;
-    }
-
-
-
-
-
-    var topAnimators = [];
-    var leftAnimators = [];
-
-
-    /**
-     * Scrolls the given element to the desired position from top
-     * 
-     * @param {Element} elem to scroll
-     * @param {Number} pos to scroll to
-     * @param {Number} time (msec) of animation
-     */
-    function scrollTop(elem, pos, time) {
-        time = time || TIME;
-        elem = elem.scroller || elem;
-
-        var animator = null;
-
-        for (var i = 0; i < topAnimators.length; i++) {
-            if (topAnimators[i].elem == elem) {
-                animator = topAnimators[i];
-                break;
-            }
-        }
-
-        if (!animator) {
-            var setter = function(val) {
-                elem.scrollTop = val;
-            }
-
-            animator = {
-                elem : elem,
-                animator : new Animator(setter)
-            };
-
-            elem.addEventListener(
-                'scroll',
-                function() {
-                    animator.animator.update(elem.scrollTop);
-                },
-                0
+            F = calculateFrames(
+                frame[0], frame[1], frame[2], target, fnum
             );
 
-            animator.animator.update(elem.scrollTop);
+            fIdx = 0;
 
-            topAnimators.push(animator);
+            if (!interval) {
+                var tick = function() {
+                    var val = F[fIdx++][0];
+                    setter(val);
+                    
+                    if (fIdx == F.length) {
+                        clearInterval(interval);
+                        interval = null_;
+                        F = [[val,0,0,0,0,0]];
+                        fIdx = 0;
+                    }
+                }
+
+                interval = setInterval(tick, DELAY);
+            }
         }
 
-        animator.animator.slide(pos, time);
+
+        /**
+         * Updates the value changed from outside
+         * 
+         * @param {Number} value
+         */
+        this.up = function(value) {
+            if (interval === null_) {
+                F[fIdx][0] = value;
+            }
+        }
+
+
+        /**
+         * Integer-based calculation of the animation frames spline
+         * 
+         * @param {Number} f0 current value
+         * @param {Number} f1 current first difference
+         * @param {Number} f2 current second difference
+         * @param {Number} target value at the end
+         * @param {Number} fnum number of frames
+         */
+        var calculateFrames = function(
+            f0, f1, f2, target, fnum
+        ) {
+            var n = fnum;
+            var n2 = n * n;
+            var n3 = n2 * n;
+            var n4 = n3 * n;
+            var n5 = n4 * n;
+
+            var f3, f4, f5;
+
+            // these magic formulae came from outer space
+            f3 = -( 9 * f2 * n2 +
+                    (36 * f1 -9 * f2) * n -
+                    36 * f1 +
+                    60 * f0 -
+                    60 * target
+                  ) / (n3 - n);
+            
+            f4 = ( 36 * f2 * n2 +
+                   (192 * f1 -36 * f2) * n -
+                   192 * f1 +
+                   360 * f0 -
+                   360 * target
+                 ) / (
+                     n4 +
+                     2 * n3 -
+                     n2 -
+                     2 * n
+                 );
+            
+            f5 = -( 60 * f2 * n2 +
+                    (360 * f1 -60 * f2) * n -
+                    360 * f1 +
+                    720 * f0 -
+                    720 * target
+                  ) / (
+                      n5 +
+                      5 * n4 +
+                      5 * n3 -
+                      5 * n2 -
+                      6 * n
+                  );
+
+            var result = [[f0, f1, f2, f3, f4, f5]];
+
+            var lastframe, frame, i, j;
+            for (i = 0; i < fnum; i++) {
+                lastframe = result[result.length - 1];
+
+                frame = [0, 0, 0, 0, 0, f5];
+
+                for (j = 4; j >= 0; j--) {
+                    frame[j] = frame[j+1] + lastframe[j];
+                }
+
+                result.push(frame);
+            }
+
+            return result;
+        }
+        
     }
 
+    // top and left animators intsances
+    var animators = {t:[], l:[]};
 
     /**
-     * Scrolls the given element to the desired position from left
+     * Returns a function which scrolls the given element to the
+     * desired position from top
      * 
-     * @param {Element} elem to scroll
-     * @param {Number} pos to scroll to
-     * @param {Number} time (msec) of animation
+     * @param {Boolean} top true to return vertical scrolling function
+     * 
+     * @returns {Function}
      */
-    function scrollLeft(elem, pos, time) {
-        time = time || TIME;
-        elem = elem.scroller || elem;
+    var scroll = function(top) {
+        
+        /**
+         * Scrolls the given element to the desired position from top
+         * 
+         * @param {Element} elem to scroll
+         * @param {Number} pos to scroll to
+         * @param {Number} time (msec) of animation
+         */
+        return function(elem, pos, time) {
+            time = time || TIME;
+            elem = elem.scroller || elem;
 
-        var animator = null;
+            var animator = null_;
+            var animatorsList = animators[top ? 't':'l'];
+            var prop = top ? scrollTop : scrollLeft;
 
-        for (var i = 0; i < leftAnimators.length; i++) {
-            if (leftAnimators[i].elem == elem) {
-                animator = leftAnimators[i];
-                break;
+            for (var i = 0; i < animatorsList.length; i++) {
+                if (animatorsList[i].elem == elem) {
+                    animator = animatorsList[i];
+                    break;
+                }
             }
-        }
 
-        if (!animator) {
-            var setter = function(val) {
-                elem.scrollLeft = val;
+            if (!animator) {
+                var setter = function(val) {
+                    elem[prop] = val;
+                }
+
+                animator = {
+                    elem : elem,
+                    animator : new Animator(setter)
+                };
+
+                elem[addEventListener](
+                    'scroll',
+                    function() {
+                        animator.animator.up(elem[prop]);
+                    },
+                    0
+                );
+                
+                animator.animator.up(elem[prop]);
+                animatorsList.push(animator);
             }
 
-            animator = {
-                elem : elem,
-                animator : new Animator(setter)
-            };
-
-            elem.addEventListener(
-                'scroll',
-                function() {
-                    animator.animator.update(elem.scrollLeft);
-                },
-                0
-            );
-            
-            animator.animator.update(elem.scrollLeft);
-
-            leftAnimators.push(animator);
+            animator.animator.s(pos, time);
         }
-
-        animator.slide(pos, time);
     }
 
 
-    exports.scrollTop = scrollTop;
-    exports.scrollLeft = scrollLeft;
+    exports[scrollTop] = scroll(true);
+    exports[scrollLeft] = scroll(false);
 }));
 
