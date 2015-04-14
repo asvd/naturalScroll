5c9e73826fa019b0e82f1d11d0ca4f13c66ed6d8
Dmitry Prokashev <heliosframework@gmail.com>
2015-04-14 23:53:03 +0200

    framedrop

--- naturalScroll.js
+++ naturalScroll.js
@@ -25,18 +25,24 @@
     var scrollTop = 'scrollTop';
     var scrollLeft = 'scrollLeft';
 
-    // returns scrollTop if argument is given, scrollLeft otherwise
+    // returns scrollTop() if argument is given, scrollLeft() otherwise
     var genScroll = function(top) {
-        var prop = top ? scrollTop : scrollLeft;
-        var dirAnimations = allAnimations[top?0:1];
 
         // exported method
         return function(elem, target, time) {
             elem = elem.scroller || elem;  // needed for intence
             time = time || 600;
 
-            var animation, tick, i = 0,
-                f0 = elem[prop], f1 = 0, f2 = 0;
+            // all animations for the particular direction
+            var dirAnimations = allAnimations[top?0:1];
+            var prop = top ? scrollTop : scrollLeft;
+
+            var animation,
+                tick,
+                i = 0,
+                f0 = elem[prop],  // current coordinate
+                f1 = 0,           // current speed
+                f2 = 0;           // current acceleration
 
             // searching for the element's animation
             for (;i < dirAnimations.length; i++) {
@@ -45,30 +51,31 @@
             }
 
             if (animation) {
+                // taking speed and acceleration from running animation
                 f1 = animation.f[1];
                 f2 = animation.f[2];
             } else {
-                // generating a new animation
-
-                // animation contains the following data:
+                // generating a new animation which contains:
                 // .e - element on which the animation is performed
-                // .f - current animation frame
-                // .l - number of remaining animation frames
-                // .i - interval function rendering a single frame
+                // .f - current animation frame data
+                // .n - remaining frames number
+                // .t - animation end timestamp
                 dirAnimations.push(animation = {e : elem});
             }
 
-            // 20 msec is the delay between the frames
-            var fnum = animation.l = Math.floor(time/20);
+            animation.t = (new Date).getTime()+time;
+
+            // total number of frames (most will be dropped though)
+            var fnum = animation.n = time;
             var fnum2 = fnum * fnum;
             var fnum3 = fnum2 * fnum;
             var f0_target = f0-target;
 
             // calculating initial frame
             animation.f = [
-                f0,  // first element is the value
-                f1,
-                f2,
+                f0,  // coordinate
+                f1,  // speed
+                f2,  // acceleration
 
                 // these magic formulae came from outer space
                 - ( 9 * f2 * fnum2 +
@@ -92,30 +99,39 @@
                 )
             ];
 
-            if (!animation.i) {
-                // creating the function for the interval,
-                // and invoking it to apply the first frame instantly
-                (tick = function(frame) {
-                    elem[prop] = (frame = animation.f)[0];
+            // creating the timeout function
+            // and invoking it to apply the first frame instantly
+            // (if the animation is already running, another timeout
+            // is launched along with the existing, which is not a
+            // problem, since we are already spam with this function
+            // every msec)
+            (tick = function(i) {
+                 while (
+                     // frames are not over
+                     animation.n &&
+                     // current frame is not yet reached
+                     animation.n > animation.t - (new Date).getTime()
+                 ) {
+                     // calculating the next frame (i+1 means i>=0)
+                     for (i = 4; i+1;) {
+                         animation.f[i] += animation.f[i--+1];
+                     }
 
-                    if (--animation.l) {
-                        // calculating the next frame
-                        // i+1 is the same as i>=0
-                        for (i = 4; i+1;) {
-                            frame[i] += frame[i--+1];
-                        }
-                    } else {
-                        // stopping animation
-                        clearInterval(animation.i);
-                        animation.i = frame[1] = frame[2] = 0;
-                    }
-                })();
+                     animation.n--;
+                 }
 
-                animation.i = setInterval(tick, 20);
-            }  // else animation is already running
+                 elem[prop] = animation.f[0];
+
+                 if (animation.n) {
+                     // scheduling the next frame
+                     setTimeout(tick, 1);
+                 } else {
+                     // stopping animation
+                     animation.r = animation.f[1] = animation.f[2] = 0;
+                 }
+            })();
         }
     }
-
 
     exports[scrollTop] = genScroll(
         exports[scrollLeft] = genScroll()
